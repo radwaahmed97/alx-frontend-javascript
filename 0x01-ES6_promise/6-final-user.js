a5647e2618c626cb36943c7c12c5b183f868710b
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-05-29 16:02:17 -0400

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,15 +2,14 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-  const resultarr= [];
-  return Promise.allSettled(promises).then((results) => {
-    results.map(({ status, value, reason }) => (
-      resultarr.push({
-        status,
-        value: status === 'rejected' ? reason.toString() : value,
-      })
-    ));
-    return resultarr;
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((values) => {
+    const arr = [];
+    for (const item of values) {
+      arr.push({ status: item.status, value: item.value || item.reason });
+    }
+    return arr;
   });
 }
