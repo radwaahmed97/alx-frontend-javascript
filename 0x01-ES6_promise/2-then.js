9eaa5eb2594f51016960880753b11cf078c6905c
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-05-29 15:30:03 -0400

    2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,7 @@
 export default function handleResponseFromAPI(promise) {
+  const response = { status: 200, body: 'success' };
   return promise
-	.then(() => ({ status: 200, body: 'success' }))
-	.catch(() => new Error())
-	.finally(() => { console.log('Got a response from the API'); });
+    .then(() => response)
+    .catch((error) => error)
+    .finally(() => console.log('Got a response from the API'));
 }
