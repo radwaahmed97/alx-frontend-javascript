c792a37727d677739679b021d663c752ca396b0e
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-05-30 17:02:57 -0400

    5-building.js

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,22 @@
+export default class Building {
+  constructor(sqft) {
+    if (
+      this.constructor !== Building
+      && typeof this.evacuationWarningMessage !== 'function'
+    ) {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
+    }
+
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Square foot must be a number');
+    }
+
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
