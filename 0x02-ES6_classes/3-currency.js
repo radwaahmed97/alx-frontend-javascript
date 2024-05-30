d9c6451c732719f8e5e8de3405ec9e3af8133ae1
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-05-30 16:57:22 -0400

    3-currency.js

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,33 @@
+export default class Currency {
+    constructor(code, name) {
+        if (typeof code !== 'string') throw new TypeError('Code must be a string');
+        if (typeof name !== 'string') throw new TypeError('Name must be a string');
+
+        this._code = code;
+        this._name = name;
+    }
+
+    // Code
+    get code() {
+        return this._code;
+    }
+
+    set code(newCode) {
+        if (typeof newCode !== 'string') throw new TypeError('Code must be a string');
+        this._code = newCode;
+    }
+
+    // Name
+    get name() {
+        return this._name;
+    }
+
+    set name(newName) {
+        if (typeof newName !== 'string') throw new TypeError('Name must be a string');
+        this._name = newName;
+    }
+
+    displayFullCurrency() {
+        return `${this._name} (${this._code})`;
+    }
+}
