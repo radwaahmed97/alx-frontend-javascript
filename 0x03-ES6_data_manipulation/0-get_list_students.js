ff71d9a5f19da86528b2e557649eb484ef2bb5be
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-06-04 13:44:51 -0400

    0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,9 @@
-export default function get_list_students() {
-    const students = [];
-    students.push({ id: 1, firstName: 'Guillaume', location: 'San Francisco' });
-    students.push({ id: 2, firstName: 'James', location: 'Columbia' });
-    students.push({ id: 5, firstName: 'Serena', location: 'San Francisco' });
-    return students;
+export default function getListStudents() {
+  const students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+
+  return students;
 }
