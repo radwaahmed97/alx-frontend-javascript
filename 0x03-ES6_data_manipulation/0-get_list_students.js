e64c3680a616054fc30f78d5670c29d0bd8f7b14
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-06-04 13:43:49 -0400

    0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,14 +1,7 @@
 export default function get_list_students() {
-    class Student {
-        constructor(id, firstName, location) {
-            this.id = id;
-            this.firstName = firstName;
-            this.location = location;
-        }
-    }
     const students = [];
-    students.push(new Student(1, "Guillaume", "San Francisco"));
-    students.push(new Student(2, "James", "Columbia"));
-    students.push(new Student(3, "Serena", "San Francisco"));
+    students.push({ id: 1, firstName: 'Guillaume', location: 'San Francisco' });
+    students.push({ id: 2, firstName: 'James', location: 'Columbia' });
+    students.push({ id: 5, firstName: 'Serena', location: 'San Francisco' });
     return students;
 }
